d32020f6f89847cad58c3b565b682769a00b0c36
Pieter Beulque <pieterbeulque@gmail.com>
2015-10-12 12:43:55 +0200

    Angular.js directive

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mr-slides
 Basic &amp; flexible slideshow where the logic is in JS &amp; the fun in CSS
+
+## Angular usage
+See [issue #1](https://github.com/mrhenry/mr-slides/issues/1).
---
+++ src/mr-slides-angular.js
@@ -0,0 +1,45 @@
+import {Component, Inject} from 'fd-angular-core';
+import TMPL from './slideshow.html!';
+
+@Component({
+    templateUrl: TMPL,
+    replace: true,
+    restrict: 'A',
+    scope: {
+        hasNavigation: '@',
+        hasPagination: '@',
+        images: '='
+    }
+})
+@Inject('$scope')
+export class MrSlidesController {
+    constructor ($scope) {
+        this.$scope = $scope;
+
+        this.$scope.images = this.images;
+        this.$scope.hasNavigation = !!(this.hasNavigation !== undefined);
+        this.$scope.hasPagination = !!(this.hasPagination !== undefined);
+
+        this.$scope.currentIndex = 0;
+
+        this.$scope.setCurrentIndex = (newIndex) => {
+            if (newIndex < 0) {
+                newIndex = 0;
+            }
+
+            if (newIndex >= this.images.length) {
+                newIndex = this.images.length - 1;
+            }
+
+            this.$scope.currentIndex = newIndex;
+        }
+
+        this.$scope.next = () => {
+            this.$scope.setCurrentIndex(this.$scope.currentIndex + 1);
+        }
+
+        this.$scope.previous = () => {
+            this.$scope.setCurrentIndex(this.$scope.currentIndex - 1);
+        }
+    }
+}
