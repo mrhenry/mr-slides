d194ef8f3076450ac48706727ec012800219f9da
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2016-02-17 14:34:47 +0100

    Added interval to angular version

--- src/mr-slides-angular.js
+++ src/mr-slides-angular.js
@@ -1,5 +1,5 @@
 import {Component, Inject} from 'fd-angular-core';
-import TMPL from './mr-slides-angular.html!';
+import TMPL from './mr-slides-angular.html!html';
 
 @Component({
     templateUrl: TMPL,
@@ -8,27 +8,30 @@ import TMPL from './mr-slides-angular.html!';
     scope: {
         hasNavigation: '@',
         hasPagination: '@',
+        intervalDelay: '=',
         images: '='
     }
 })
 @Inject('$scope')
 export class MrSlidesController {
     constructor ($scope) {
+        this.intervalId = null;
         this.$scope = $scope;
 
         this.$scope.images = this.images;
         this.$scope.hasNavigation = !!(this.hasNavigation !== undefined);
         this.$scope.hasPagination = !!(this.hasPagination !== undefined);
+        this.$scope.intervalDelay = this.intervalDelay || false;
 
         this.$scope.currentIndex = 0;
 
         this.$scope.setCurrentIndex = (newIndex) => {
             if (newIndex < 0) {
-                newIndex = 0;
+                newIndex = this.images.length - 1;
             }
 
             if (newIndex >= this.images.length) {
-                newIndex = this.images.length - 1;
+                newIndex = 0;
             }
 
             this.$scope.currentIndex = newIndex;
@@ -40,6 +43,27 @@ export class MrSlidesController {
 
         this.$scope.previous = () => {
             this.$scope.setCurrentIndex(this.$scope.currentIndex - 1);
+        }
+
+        /**
+         * Start interval
+         */
+        this.$scope.start = () => {
+            this.intervalId = setInterval(() => {
+                this.$scope.next();
+            }, this.intervalDelay);
+        }
+
+        /**
+         * Stop interval
+         */
+        this.$scope.stop = () => {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+
+        if (this.$scope.intervalDelay) {
+            this.$scope.start();
         }
     }
 }
